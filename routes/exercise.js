65fbcbc7c6eaae4d319f9f9bc439af132896d38b
khanjasir90 <khanmohdjasir@gmail.com>
2022-01-09 04:10:11 +0530

    Project Completed and Added Readme file

--- routes/exercise.js
+++ routes/exercise.js
@@ -3,7 +3,9 @@ const exercise = require('express')
 const router = express.Router()
 const exerciseData = require('../data/exerciseData')
 const User = require('../api/models/userModel')
-const Contest = require('../api/models/contestModel') 
+const Contest = require('../api/models/contestModel')
+const mongoose = require('mongoose')
+
 router.get("/contest/:id",(req,res)=> {
     const id = req.params.id;
     let itemYouWant = null;
@@ -16,6 +18,18 @@ router.get("/contest/:id",(req,res)=> {
 })
 
 router.get('/streak/:id', async(req,res)=>{
+    User.findOneAndUpdate(
+        {"_id" : mongoose.Types.ObjectId(req.session.userInfo._id)},{
+            $inc : {streak: +1}
+        },(err,status) => {
+            if(err) {
+                console.log(err)
+            }else{
+                console.log(status)
+            }
+        }
+    )
+    res.redirect('/contest')
 })
 
 
